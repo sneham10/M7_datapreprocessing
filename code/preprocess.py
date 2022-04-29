0cdb7ea6a4df474b1f937efb815eb68dd3d74161
sneham10 <sneha.muralidharan10@gamil.com>
2022-04-29 12:41:41 +0530

    done

---
+++ code/preprocess.py
@@ -0,0 +1,19 @@
+import pandas as pd
+df=pd.read_csv('sales.csv')
+#printing info
+print(df.info())
+#fix the formate like date
+df['Order Date']=pd.to_datetime(df['Order Date'])
+#fix the formate like date
+df['Ship Date']=pd.to_datetime(df['Ship Date'])
+#checking presence of duplicate
+print(df.duplicated())
+#removing country 
+del df['Country']
+del df['Total Revenue']
+del df['Unit Price']
+df['Order Date'] = pd.to_datetime(df['Order Date'])
+df['year'] = df['Order Date'].dt.year
+print(df)
+#saving in another file
+df.to_csv('cleaned1.csv')
